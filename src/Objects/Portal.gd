15aa0ed9e83dea8966250b80ba5b2764319208ee
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2020-10-15 22:36:56 +0530

    Deleted scene 2

--- src/Objects/Portal.gd
+++ src/Objects/Portal.gd
@@ -1,20 +1,17 @@
 tool
 extends Area2D
-
-
 onready var anim_player: AnimationPlayer= $AnimationPlayer 
-export var path_to_level: String
+#export var path_to_level: String
 
 func _on_Portal_body_entered(body: PhysicsBody2D) -> void:
 	teleport()
 
-func _get_configuration_warning() -> String:
-
-	return "The next scene property cant be empty" if not path_to_level else ""
+#func _get_configuration_warning() -> String:
+	#return "The next scene property cant be empty" if not path_to_level else ""
 
 func teleport() -> void:
 	anim_player.play("Fade in")
 	yield(anim_player, "animation_finished")
-	var scene:= load(path_to_level)
-	get_tree().change_scene_to(scene)
-
+	#var next_scene :=load(path_to_level)
+	#get_tree().change_scene_to(next_scene)
+	
