6267bd70b9e892121bcb6278789052ef2bae4a61
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2020-10-15 20:04:16 +0530

    New lvl added

--- src/Objects/Portal.gd
+++ src/Objects/Portal.gd
@@ -3,17 +3,18 @@ extends Area2D
 
 
 onready var anim_player: AnimationPlayer= $AnimationPlayer 
-export var next_scene: PackedScene
+export var path_to_level: String
 
 func _on_Portal_body_entered(body: PhysicsBody2D) -> void:
 	teleport()
 
 func _get_configuration_warning() -> String:
 
-	return "The next scene property cant be empty" if not next_scene else ""
+	return "The next scene property cant be empty" if not path_to_level else ""
 
 func teleport() -> void:
 	anim_player.play("Fade in")
 	yield(anim_player, "animation_finished")
-	get_tree().change_scene_to(next_scene)
+	var scene:= load(path_to_level)
+	get_tree().change_scene_to(scene)
 
