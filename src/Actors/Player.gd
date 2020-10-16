c99fefbbdb98bbf283463f699e82a55071965a4c
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2020-10-16 15:12:02 +0530

    Sprite run animation added

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -15,6 +15,9 @@ func _physics_process(delta: float) -> void:
 	velocity= move_and_slide(velocity, FLOOR_NORMAL)
 
 func get_direction() ->Vector2:
+	 if Input.get_action_strength("move_right"):$AnimationPlayer.play("run")
+	 else: $AnimationPlayer.stop()
+	
 	 return Vector2(
 	Input.get_action_strength("move_right")- Input.get_action_strength("move_left"),
 		-1.0 if Input.get_action_strength("jump") and is_on_floor() else 0.0
